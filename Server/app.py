0fc9f8a4e30389cd98190865e1fae9479ca0fe26
SunSet0418 <coffee@soylatte.kr>
2017-11-03 19:10:09 +0900

    edit hangul

--- Server/app.py
+++ Server/app.py
@@ -9,7 +9,7 @@ def index():
     print(getdata)
     string = getdata.strip('\\r\\n')
     split = string.split(' ')
-    s = "%04d년 %02d월 %02d일  %02d시 %02d분 %02d초" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+    s = "%04d %02d %02d  %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
     returndata = {'Temperature' : split[0], 'Humidity' : split[1], 'Time' : s, 'Sound' : split[2]}
     print(returndata)
     return jsonify(returndata)
